81ed713195fc7bf3469e89472ae5326bcb3b5215
Renan Cosme Pinto <renan.pinto@aec.com.br>
2017-03-09 19:55:34 -0300

    Project created

---
+++ AssemblyInformation/AssemblyInformation.cs
@@ -0,0 +1,56 @@
+﻿namespace AssemblyInformation
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;    
+
+    public class AssemblyInformation
+    {
+        private Assembly _assembly = null;
+
+        public AssemblyInformation(Assembly assembly)
+        {
+            _assembly = assembly;
+        }
+
+        public string Title
+        {
+            get { return GetInformationAtribute<AssemblyTitleAttribute>().Title; }
+        }
+
+        public string Description
+        {
+            get { return GetInformationAtribute<AssemblyDescriptionAttribute>().Description; }
+        }
+
+        public string Company
+        {
+            get { return GetInformationAtribute<AssemblyCompanyAttribute>().Company; }
+        }
+
+        public string Product
+        {
+            get { return GetInformationAtribute<AssemblyProductAttribute>().Product; }
+        }
+
+        public string Copyright
+        {
+            get { return GetInformationAtribute<AssemblyCopyrightAttribute>().Copyright; }
+        }
+
+        public string Trademark
+        {
+            get { return GetInformationAtribute<AssemblyTrademarkAttribute>().Trademark; }
+        }
+
+        public string Version
+        {
+            get { return GetInformationAtribute<AssemblyFileVersionAttribute>().Version; }
+        }
+
+        private T GetInformationAtribute<T>() where T : Attribute
+        {
+            return _assembly.GetCustomAttributes<T>().FirstOrDefault();
+        }
+    }
+}
---
+++ AssemblyInformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AssemblyInformation Title")]
+[assembly: AssemblyDescription("Description of AssemblyInformation product")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Renan Cosme Pinto Company")]
+[assembly: AssemblyProduct("AssemblyInformation Library returns Title, Description, etc...")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("Product Trademark informations")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93ba7941-0b0f-4318-a356-0b8a6f17867b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
