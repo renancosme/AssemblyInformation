31b9a8cfe9350f69676970d4119a2955b4100788
Renan Cosme Pinto <renan.pinto@aec.com.br>
2017-03-10 19:15:51 -0300

    Remove Console Application and create a Class Library

--- AssemblyInformations/Properties/AssemblyInfo.cs
+++ AssemblyInformations/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AssemblyInformation Title")]
-[assembly: AssemblyDescription("Description of AssemblyInformation product")]
+[assembly: AssemblyTitle("AssemblyInformations")]
+[assembly: AssemblyDescription("Provide a class with properties that displays application assembly information (title, description, company, product, copyright, trademark and/or version number).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Renan Cosme Pinto Company")]
-[assembly: AssemblyProduct("AssemblyInformation Library returns Title, Description, etc...")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
-[assembly: AssemblyTrademark("Product Trademark informations")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInformations")]
+[assembly: AssemblyCopyright("Copyright © Renan Cosme Pinto 2017")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("93ba7941-0b0f-4318-a356-0b8a6f17867b")]
+[assembly: Guid("0179093c-5fa5-4975-8df6-2ac08e5e3d88")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
